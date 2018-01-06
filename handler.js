045abb91f3e5562036ad28a0143bd5b004aaa54b
Jonata Weber <jonataa@gmail.com>
2018-01-06 16:13:05 -0200

    create yml file in s3 bucket

--- handler.js
+++ handler.js
@@ -23,7 +23,7 @@ module.exports.crawlerRemoteJobs = (event, context, callback) => {
       Key: "jobs.json", 
       ACL: 'public-read',
       ServerSideEncryption: "AES256", 
-      Tagging: "key1=value1&key2=value2"
+      // Tagging: "key1=value1&key2=value2"
      };
   
      s3.putObject(params, function(err, data) {
@@ -44,4 +44,31 @@ module.exports.crawlerRemoteJobs = (event, context, callback) => {
   };
 
   callback(null, response);
+}
+
+module.exports.parseJsonJobsToYmal = (event, context, callback) => {
+  const S3PutObject = require('./lib/S3').putObject
+      , YAML = require('yamljs')
+  ;
+
+  let yamlString = YAML.stringify({ hello: 'world' });
+  
+  S3PutObject(yamlString, 'jobs.yml', 'public-read', function(err, data) {
+    if (err) console.log('error', err, err.stack); // an error occurred
+    else     console.log('success', data);         // successful response
+  });
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'It works!'
+    }),
+  };
+
+  callback(null, response);
+
+  console.log('ParseJsonToYmal LogScheduledEvent');
 }
